7e54a9d982cfed5263624adc41a1bbf6223d3acc
JUAN CARLOS HERNANDEZ CAMPOS <1402076@mobilityado.com>
2023-02-28 23:05:23 -0600

    First commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,36 @@
+
+import { GifItem } from "../components/GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+export const GifGrid = ({category}) => {
+
+    const {images, isLoading} = useFetchGifs(category);
+
+
+
+    return (
+        <>
+            <h3>{category}</h3>
+            {
+                isLoading ? (<h2>Cargando...</h2>) : null
+
+            }
+            <div className="card-grid">
+                {
+ 
+                    images.map( (image) => (
+
+                        <GifItem 
+                        
+                        key={image.id}
+                        title = {image.title}
+                        url = {image.url}
+                        />             
+
+                    ))
+                }
+            </div>
+        
+        </>
+    )
+}
